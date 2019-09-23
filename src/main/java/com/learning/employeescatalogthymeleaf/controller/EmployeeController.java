33478136ee1f09cd068197bd3e1e91bf3e3abd44
poymanov <n.poymanov@gmail.com>
2019-09-23 23:02:59 +0300

    Added action to saving new employee

--- src/main/java/com/learning/employeescatalogthymeleaf/controller/EmployeeController.java
+++ src/main/java/com/learning/employeescatalogthymeleaf/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.learning.employeescatalogthymeleaf.controller;
 
+import com.learning.employeescatalogthymeleaf.entity.Employee;
 import com.learning.employeescatalogthymeleaf.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/employees")
@@ -17,9 +20,21 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
-    @GetMapping("/list")
-    public String listEmployees(Model model) {
+    @GetMapping("/index")
+    public String index(Model model) {
         model.addAttribute("employees", employeeService.findAll());
-        return "list-employees";
+        return "employees/index";
+    }
+
+    @GetMapping("/new")
+    public String newEmployee(Model model) {
+        model.addAttribute("employee", new Employee());
+        return "employees/form";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute Employee employee) {
+        employeeService.save(employee);
+        return "redirect:/employees/index";
     }
 }
--- src/main/java/com/learning/employeescatalogthymeleaf/dao/EmployeeRepository.java
+++ src/main/java/com/learning/employeescatalogthymeleaf/dao/EmployeeRepository.java
@@ -3,6 +3,8 @@ package com.learning.employeescatalogthymeleaf.dao;
 import com.learning.employeescatalogthymeleaf.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+import java.util.List;
 
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    List<Employee> findAllByOrderByLastNameAsc();
 }
